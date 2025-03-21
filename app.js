721053cd2d16ef480f62d0b50e9fcbcd1e4c80a9
CristofherJT <156453145+CristofherJT@users.noreply.github.com>
2025-03-20 21:27:14 -0500

    Update app.js
    Added validation for registration form

--- app.js
+++ app.js
@@ -34,8 +34,62 @@ $('#btnLogin').on('click', function(){
     }
 })
 
+$('#btnRegister').on('click', function(){
+    const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+
+    let strFirstName = $('#txtFirstName').val()
+    let strLastName = $('#txtLastName').val()
+    let strEmail = $('#txtUsername').val()
+    let strPassword = $('#txtPassword').val()
+
+    let blnError = false
+    let strMessage = ''
+
+    if(strFirstName.length < 1){
+        blnError = true
+        strMessage += '<p class="mb-0 mt-0">Please enter a valid first name.</p>'
+    }
+
+    if(strLastName.length < 1){
+        blnError = true
+        strMessage += '<p class="mb-0 mt-0">Please enter a valid last name.</p>'
+    }
+
+    if(!regEmail.test(strEmail)){
+        blnError = true
+        strMessage += '<p class="mb-0 mt-0">Please enter a valid email.</p>'
+    }
+
+    if(strPassword.length < 1){
+        blnError = true
+        strMessage += '<p class="mb-0 mt-0">Please enter a valid password.</p>'
+    }
+
+    if(blnError == true){
+        Swal.fire({
+            title: "Oh no, you have an error!",
+            html: strMessage,
+            icon: "error"
+        });
+    }
+
+    if(blnError == false){
+        Swal.fire({
+            title: "Registration Successful!",
+            html: strMessage,
+            icon: "success"
+        });
+    }
+})
+
 $('#btnSwapRegister').on('click', function(){
     $('#frmLogin').slideUp(function(){
         $('#frmReg').slideDown()
+    })
+})
+
+$('#btnSwapLogin').on('click', function(){
+    $('#frmReg').slideUp(function(){
+        $('#frmLogin').slideDown()
     })
 })
